20179d07a3cb6ecf3f194c7f9fa543a41707ec20
noripi10 <sgym.snk@gmail.com>
2022-08-10 23:45:35 +0900

    update item ui

--- src/components/parts/SwitchBotItem.tsx
+++ src/components/parts/SwitchBotItem.tsx
@@ -4,7 +4,7 @@ import { Box, Button, Center, Flex, HStack, Stack, Text, useColorModeValue, VSta
 
 import { BiDevices } from 'react-icons/bi';
 
-import { DEVICE_POWER_OFF, DEVICE_POWER_ON, MY_SWITC_BOT_TOKEN } from '@src/libs/constants';
+import { DEVICE_POWER_OFF, DEVICE_POWER_ON, DEVICE_TYPE_HUB_MINI, MY_SWITC_BOT_TOKEN } from '@src/libs/constants';
 import { getStorageData } from '@src/libs/storage';
 import { Device, RemoteDevice } from '@src/pages/background/fetcher';
 
@@ -15,17 +15,14 @@ export const DeviceItem = ({
   device: Device | RemoteDevice;
   setDeviceStatus: (deviceType: 'device' | 'remoteDevice', deviceId: string, status: number) => void;
 }) => {
-  const [status, setStatus] = useState('');
-  const statusColor = useMemo(() => {
-    return /success/.test(status) || status === '' ? 'green.400' : 'red.400';
-  }, [status]);
+  const isDevice = 'deviceType' in device;
+  const isRemoteDevice = !('deviceType' in device);
+  const mark = isDevice ? device.deviceMark : device.hubDeviceMark;
 
-  // TODO Hub Miniだとステータスがうまく取得できない(body無しでstatus:100が返却される)
-  const iconStatusColor = useMemo(
-    () =>
-      'deviceType' in device && device.deviceType == 'Hub Mini' ? '#ababab' : device.online ? '#4ba01d' : '#c03a3a',
-    [device.online]
-  );
+  const [commandStatus, setCommandStatus] = useState('');
+  const commandStatusColor = useMemo(() => {
+    return /success/.test(commandStatus) || commandStatus === '' ? 'green.400' : 'red.400';
+  }, [commandStatus]);
 
   const onPowerToggle = async (type: typeof DEVICE_POWER_ON | typeof DEVICE_POWER_OFF) => {
     const token = await getStorageData(MY_SWITC_BOT_TOKEN);
@@ -33,12 +30,12 @@ export const DeviceItem = ({
     const command = type === DEVICE_POWER_ON ? 'turnOn' : 'turnOff';
 
     chrome.runtime.sendMessage({ type, deviceId: device.deviceId, token, command }, (response) => {
-      console.log({ response });
+      // console.log({ response });
       if (response) {
-        setStatus(response.message);
+        setCommandStatus(response.message);
         startTransition(() => {
-          setTimeout(() => setStatus(''), 1500);
-          setDeviceStatus('deviceType' in device ? 'device' : 'remoteDevice', device.deviceId, response.statusCode);
+          setTimeout(() => setCommandStatus(''), 1500);
+          setDeviceStatus(isDevice ? 'device' : 'remoteDevice', device.deviceId, response.statusCode);
         });
       }
     });
@@ -57,15 +54,18 @@ export const DeviceItem = ({
           flex={1}
           flexDirection={'column'}
           bgColor={useColorModeValue('gray.200', 'gray.700')}
-          borderRadius={8}
+          borderRadius={6}
         >
           <Box pb={'2'}>
-            {/* TODO アイコンをTypeで変える */}
-            <BiDevices size={32} color={iconStatusColor} />
+            <BiDevices size={32} color={device.statusColor} />
           </Box>
 
           <Box position={'absolute'} bottom={1}>
-            <Text color={iconStatusColor}>{device.online ? 'online' : 'offline'}</Text>
+            <Text color={device.statusColor}>{device.status}</Text>
+          </Box>
+
+          <Box position={'absolute'} top={1} left={1}>
+            <Text color={useColorModeValue('rgba(0,0,0,0.4)', 'rgba(255,255,255, 0.6)')}>{mark}</Text>
           </Box>
         </Center>
 
@@ -94,8 +94,8 @@ export const DeviceItem = ({
         </VStack>
       </HStack>
       <Flex h={1} alignItems='center' justifyContent='center' py={1}>
-        <Text fontWeight={'bold'} color={statusColor}>
-          {status ?? ''}
+        <Text fontWeight={'bold'} color={commandStatusColor}>
+          {commandStatus ?? ''}
         </Text>
       </Flex>
     </Stack>
